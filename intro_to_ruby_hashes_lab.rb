4337297b87b4b2d129015d2d6ef382f15cd446f9
Daniel Phillips <danphi@gmail.com>
2020-01-19 05:55:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned => 25,
-        :key2 => "blah",
+        :two_pieces_owned => 50,
         :key3 => "blah",
         :key4 => "blah"
       },
